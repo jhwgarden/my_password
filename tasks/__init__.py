e70a13b16aeba57bb90cf57fdeb7d310dcaad8a0
Justin Worrall <justin.worrall@gmail.com>
2013-11-25 20:26:30 +0000

    completed encode, search tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -9,8 +9,6 @@ class BaseTask:
     def initialise(self, *args, **kwargs):
         pass
 
-from lib.encoder import encode, decode
-
 EncodedFields=["password"]
 
 SecretKeyFile="tmp/secretkey.txt"
@@ -20,6 +18,20 @@ def load_secretkey():
         return re.sub("\\s", "", file(SecretKeyFile).read())
     except:
         raise RuntimeError("Secret key does not exist")
+
+def encode_accounts(accounts, secretkey):
+    from lib.encoder import encode
+    for account in accounts:
+        for attr in EncodedFields:
+            if account[attr]:
+                account[attr]=encode(account[attr], secretkey)
+
+def decode_accounts(accounts, secretkey):
+    from lib.encoder import decode
+    for account in accounts:
+        for attr in EncodedFields:
+            if account[attr]:
+                account[attr]=decode(account[attr], secretkey)
 
 class EncodeTask(BaseTask):
 
@@ -38,10 +50,7 @@ class EncodeTask(BaseTask):
     def run(self, *args, **kwargs):        
         secretkey=load_secretkey()
         accounts=yaml.load(file(self.src).read())
-        for account in accounts:
-            for attr in EncodedFields:
-                if account[attr]:
-                    account[attr]=encode(account[attr], secretkey)
+        encode_accounts(accounts, secretkey)
         dest=file("tmp/encoded.yaml", 'w')
         dest.write(yaml.safe_dump(accounts, default_flow_style=False))
         dest.close()
@@ -50,13 +59,23 @@ class SearchTask(BaseTask):
 
     @classmethod
     def validate(self, *args, **kwargs):
-        pass
+        if len(args) < 2:
+            raise RuntimeError("Please enter src, pattern")
+        if not os.path.isfile(args[0]):
+            raise RuntimeError("Src file not found")        
+        if not args[0].endswith("yaml"):
+            raise RuntimeError("Src must be a yaml file")
 
     def initialise(self, *args, **kwargs):
-        pass
+        self.src, self.pattern = args[:2]
 
     def run(self, *args, **kwargs):
-        print "search"
+        secretkey=load_secretkey()
+        accounts=yaml.load(file(self.src).read())
+        decode_accounts(accounts, secretkey)
+        print yaml.safe_dump([account for account in accounts
+                              if re.search(self.pattern, account["name"], re.I)!=None],
+                             default_flow_style=False)
 
 class AddTask(BaseTask):
 
